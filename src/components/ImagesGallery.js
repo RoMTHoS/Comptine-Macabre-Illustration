a926fa603e6acb5b5a6a24db6a25574223c1914d
Matheos Romain <romain.matheos.31@gmail.com>
2022-03-03 18:47:47 +0100

    Horizontal Scroll Mobile

--- src/components/ImagesGallery.js
+++ src/components/ImagesGallery.js
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import { useGesture } from "react-use-gesture";
+import React, { useContext, useEffect, useState } from "react";
 import { IllustrationStoryContext } from "../contexts/illustrationStoryContext";
 import FirebaseFirestore from "../FirebaseFirestore";
 
