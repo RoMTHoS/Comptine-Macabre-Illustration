b17e4d387e74ec9347fc4d4c153ef7fbbcab6f2f
Matheos Romain <romain.matheos.31@gmail.com>
2022-03-04 11:59:09 +0100

    Horizontal Scroll Mobile

--- src/pages/Oeuvres.js
+++ src/pages/Oeuvres.js
@@ -19,7 +19,7 @@ const Oeuvres = () => {
     {
       onDrag: ({ offset: [dx] }) => {
         if (dx < 0) {
-          setMoove((moove) => ({ ...moove, x: dx * 0.25 + "px" }));
+          setMoove((moove) => ({ ...moove, x: dx * 2 + "px" }));
           console.log(dx);
         } else if (dx >= 0) {
           setMoove((moove) => ({ ...moove, x: 1 + "px" }));
