7e51ae213fd8fdac563fbaf8165dbd1d1576769f
Matheos Romain <romain.matheos.31@gmail.com>
2022-03-04 09:18:14 +0100

    Horizontal Scroll Mobile

--- src/pages/Oeuvres.js
+++ src/pages/Oeuvres.js
@@ -18,7 +18,7 @@ const Oeuvres = () => {
   useGesture(
     {
       onDrag: ({ offset: [dx] }) => {
-        setMoove((moove) => ({ ...moove, x: dx * 20 + "px" }));
+        setMoove((moove) => ({ ...moove, x: dx + "px" }));
         console.log(dx);
       },
     },
